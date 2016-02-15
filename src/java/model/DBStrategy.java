5fcf67aabe806d61927b60a129533050c2cd2ef9
tjcel <tjcel@TJComputer>
2016-02-15 16:53:40 -0600

    just got it to work MySql

--- src/java/model/DBStrategy.java
+++ src/java/model/DBStrategy.java
@@ -2,6 +2,8 @@
 package model;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 
 public interface DBStrategy {
@@ -10,4 +12,6 @@ public interface DBStrategy {
             throws ClassNotFoundException, SQLException;
     
     public abstract void closeConnection() throws SQLException;
+    public List<Map<String,Object>> findAllRecords(String tableName,
+            int maxRecords) throws SQLException;
 }
--- src/java/model/MySqlDBStrategy.java
+++ src/java/model/MySqlDBStrategy.java
@@ -2,7 +2,11 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
 
 /**
  *
@@ -20,7 +24,57 @@ public class MySqlDBStrategy implements DBStrategy {
         
     }
     
+    @Override
     public void closeConnection() throws SQLException {
         conn.close();
     }
+    
+    /**
+     * Make sure you open and close connection when using this method
+     * Future optimization may include change the return type as an array
+     * @param tableName
+     * @param maxRecords - limits records found to first maxRecords or if maxRecords is
+     * zero (0) then no limit
+     * @return 
+     */
+    
+    @Override
+    public List<Map<String,Object>> findAllRecords(String tableName,
+            int maxRecords) throws SQLException{
+        
+        String sql;
+        if(maxRecords == 1) {
+            sql = "select * from " + tableName;
+        }else { 
+            sql = "select * from " + tableName + " limit " + maxRecords;
+        }
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        ResultSetMetaData rsmd= rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        List<Map<String,Object>> records = new ArrayList<>();
+        
+        while(rs.next()) {
+            Map<String, Object> record = new HashMap<>();
+            for(int colNo = 1; colNo <= columnCount+1; colNo++){
+                Object colData = rs.getObject(colNo);
+                String colName = rsmd.getColumnName(colNo);
+                record.put(colName, colData);
+            }
+            records.add(record);
+        }
+        return records;
+        
+    }
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        DBStrategy db = new MySqlDBStrategy();
+        db.openConnection("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/book", "root", "admin");
+        List<Map<String,Object>> rawData = db.findAllRecords("author", 0);
+        db.closeConnection();
+        
+        System.out.println(rawData);
+    }
+
+    
 }
